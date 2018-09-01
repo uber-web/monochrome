c08d7f4c336eaf6553a7835ebb410d200fbd020c
KahWee Teng <kahwee@uber.com>
2018-08-31 17:02:17 -0700

    feat(flow): Add flow

--- src/drag-drop-list/drag-drop-list-item.js
+++ src/drag-drop-list/drag-drop-list-item.js
@@ -1,5 +1,6 @@
+// @flow
 /* global window */
-import React, {PureComponent} from 'react';
+import * as React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
@@ -9,14 +10,33 @@ const noop = () => {};
 
 const TRANSITION = 300;
 
-export default class DragDropListItem extends PureComponent {
+export type DragPos = {
+  deltaX: number,
+  deltaY: number
+};
+export type Prop = {
+  className: string,
+  title: React.Node | string,
+  removed: boolean,
+  onDragStart: Function,
+  onDragMove: Function,
+  onDragEnd: Function,
+  children?: React.Node
+};
+export type State = {
+  isDragging: boolean,
+  isActive: boolean,
+  width: number,
+  height: number,
+  dragStartOffset: {left: number, top: number},
+  dragPos: DragPos | null
+};
+export default class DragDropListItem extends React.PureComponent<Prop, State> {
+  _container: any;
 
   static propTypes = {
     className: PropTypes.string,
-    title: PropTypes.oneOfType([
-      PropTypes.node,
-      PropTypes.string
-    ]),
+    title: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
     removed: PropTypes.bool,
     onDragStart: PropTypes.func,
     onDragMove: PropTypes.func,
@@ -31,14 +51,14 @@ export default class DragDropListItem extends PureComponent {
     onDragEnd: noop
   };
 
-  state = {
+  state: State = {
     isDragging: false,
     isActive: false,
     width: 0,
     height: 0,
     dragStartOffset: {left: 0, top: 0},
     dragPos: null
-  };;
+  };
 
   componentWillUnmount() {
     window.clearTimeout(this._timer);
@@ -50,7 +70,7 @@ export default class DragDropListItem extends PureComponent {
     return this._container.getBoundingClientRect();
   }
 
-  _onDragStart = (evt) => {
+  _onDragStart = evt => {
     const container = this._container;
 
     this.setState({
@@ -63,14 +83,14 @@ export default class DragDropListItem extends PureComponent {
     });
 
     this.props.onDragStart(evt);
-  }
+  };
 
-  _onDragMove = (evt) => {
+  _onDragMove = (evt: DragPos) => {
     this.setState({dragPos: evt});
     this.props.onDragMove(evt);
-  }
+  };
 
-  _onDragEnd = (evt) => {
+  _onDragEnd = evt => {
     this.setState({
       isDragging: false,
       dragStartOffset: this._container.getBoundingClientRect(),
@@ -78,13 +98,10 @@ export default class DragDropListItem extends PureComponent {
     });
 
     // When transition is done, remove z-index and drop shadow
-    this._timer = window.setTimeout(
-      () => this.setState({isActive: false}),
-      TRANSITION
-    );
+    this._timer = window.setTimeout(() => this.setState({isActive: false}), TRANSITION);
 
     this.props.onDragEnd(evt);
-  }
+  };
 
   renderTitle() {
     const {title} = this.props;
@@ -93,10 +110,7 @@ export default class DragDropListItem extends PureComponent {
 
   renderMover(children) {
     return (
-      <Draggable
-        onStart={this._onDragStart}
-        onDrag={this._onDragMove}
-        onEnd={this._onDragEnd}>
+      <Draggable onStart={this._onDragStart} onDrag={this._onDragMove} onEnd={this._onDragEnd}>
         {children}
       </Draggable>
     );
@@ -106,30 +120,33 @@ export default class DragDropListItem extends PureComponent {
     const {className} = this.props;
     const {isDragging, isActive, width, height, dragPos, dragStartOffset} = this.state;
 
-    const contentStyle = isActive ? {
-      boxSizing: 'border-box',
-      position: 'fixed',
-      left: dragStartOffset.left + dragPos.deltaX,
-      top: dragStartOffset.top + dragPos.deltaY,
-      width,
-      height,
-      transition: isDragging ? undefined : `all ${TRANSITION}ms`
-    } : {};
+    const contentStyle = isActive
+      ? {
+          boxSizing: 'border-box',
+          position: 'fixed',
+          left: dragStartOffset.left + dragPos.deltaX,
+          top: dragStartOffset.top + dragPos.deltaY,
+          width,
+          height,
+          transition: isDragging ? undefined : `all ${TRANSITION}ms`
+        }
+      : {};
 
-    const contentClassName =
-      classnames('mc-drag-drop-list--item-content', className);
+    const contentClassName = classnames('mc-drag-drop-list--item-content', className);
 
     const title = this.renderTitle();
 
     return title ? (
-      <div className={contentClassName} style={contentStyle} >
-        {this.renderMover(<div className="mc-drag-drop-list--item-title" >{title}</div>)}
+      <div className={contentClassName} style={contentStyle}>
+        {this.renderMover(<div className="mc-drag-drop-list--item-title">{title}</div>)}
         {this.props.children}
       </div>
-    ) : this.renderMover(
-      <div className={contentClassName} style={contentStyle} >
-        {this.props.children}
-      </div>
+    ) : (
+      this.renderMover(
+        <div className={contentClassName} style={contentStyle}>
+          {this.props.children}
+        </div>
+      )
     );
   }
 
@@ -137,10 +154,13 @@ export default class DragDropListItem extends PureComponent {
     const {removed} = this.props;
     const {isDragging, isActive, width, height} = this.state;
 
-    const containerClassName =
-      classnames('mc-drag-drop-list--item', {active: isActive}, {dragging: isDragging});
+    const containerClassName = classnames(
+      'mc-drag-drop-list--item',
+      {active: isActive},
+      {dragging: isDragging}
+    );
 
-    const placeholderClassName = classnames('mc-drag-drop-list--placeholder', {removed})
+    const placeholderClassName = classnames('mc-drag-drop-list--placeholder', {removed});
 
     const placeholderStyle = {
       boxSizing: 'border-box',
@@ -150,14 +170,15 @@ export default class DragDropListItem extends PureComponent {
     };
 
     return (
-      <div className={containerClassName}
+      <div
+        className={containerClassName}
         ref={ref => {
           this._container = ref;
-        }} >
+        }}
+      >
         {this.renderContent()}
         {isActive && <div className={placeholderClassName} style={placeholderStyle} />}
       </div>
     );
   }
-
 }
