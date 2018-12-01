5570f89e897b3d0130cfb3e746bd70b0feb2972c
KahWee Teng <262105+kahwee@users.noreply.github.com>
2018-12-01 14:08:05 -0800

    Import theme from separate file (#6)

--- src/shared/theme.js
+++ src/shared/theme.js
@@ -1,41 +1,7 @@
 import React from 'react';
+import {theme} from './themes';
 
-const ThemeContext = React.createContext({
-  /* Colors */
-  background: '#FFF',
-  backgroundAlt: '#F8F8F9',
-  backgroundInvert: '#000',
-
-  controlColorPrimary: '#A6A6A5',
-  controlColorHovered: '#1C1B1B',
-  controlColorActive: '#11939A',
-  controlColorDisabled: '#D6D6D5',
-
-  textColorPrimary: '#1C1B1B',
-  textColorSubtle: '#A6A5A5',
-  textColorInvert: '#F8F8F9',
-  textColorDisabled: '#D6D6D5',
-
-  /* Typography */
-  fontFamily: "'Helvetica Neue', Helvetica, Arial, sans-serif",
-  fontSize: 12,
-  lineHeight: 1.5,
-
-  /* Effects */
-  transitionDuration: '300ms',
-  transitionTimingFunction: 'ease',
-
-  shadow: '0 2px 4px 0 rgba(0, 0, 0, 0.15)',
-
-  /* Layout */
-  controlSize: 18,
-
-  spacingTiny: 4,
-  spacingSmall: 8,
-  spacingNormal: 12,
-  spacingLarge: 24,
-  spacingHuge: 48
-});
+const ThemeContext = React.createContext(theme);
 
 export const ThemeProvider = ThemeContext.Provider;
 
@@ -54,7 +20,7 @@ export function withTheme(Component) {
     render() {
       return (
         <ThemeContext.Consumer>
-          {theme => <Component {...this.props} theme={theme} />}
+          {_theme => <Component {...this.props} theme={_theme} />}
         </ThemeContext.Consumer>
       );
     }
