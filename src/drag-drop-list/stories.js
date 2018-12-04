4493f24d2a8f3cc495cf2fba35b1574575f15cb1
KahWee Teng <kahwee@uber.com>
2018-12-04 15:03:54 -0800

    Example to show how storybook can import a README

--- src/drag-drop-list/stories.js
+++ src/drag-drop-list/stories.js
@@ -3,7 +3,13 @@
 
 import {storiesOf} from '@storybook/react';
 import examples from './examples';
+import README from './README.md';
 
 Object.entries(examples).forEach(([description, example]) =>
-  storiesOf('Form control', module).add(description, example)
+  storiesOf('DragDropList', module).add(description, example, {
+    info: {
+      header: true,
+      text: README
+    }
+  })
 );
